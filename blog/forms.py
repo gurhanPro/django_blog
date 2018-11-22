9ebb77767c04a2a05e02ba3baf06e6b688c52b8a
Ziyaad Ahmed Gurhan <ziyaadyare@gmail.com>
2018-11-23 00:06:30 +0200

    Created Views and Url Templates

--- blog/forms.py
+++ blog/forms.py
@@ -9,7 +9,7 @@ class PostForm(forms.ModelForm):
 
         widgets = {
             'title':forms.TextInput(attrs={'class':'textinputclass'}),
-            'text':forms.TextArea(attrs={'class':'editable medium-editor-textarea postcontent'})
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea postcontent'})
 
         }
 
@@ -20,7 +20,7 @@ class CommentForm(forms.ModelForm):
         fields= ('author', 'text')
 
         widgets = {
-            'author':forms.TextInput(attrs={'class':'textinputclass'})
-            'text':forms.TextArea(attrs={'class':'editable medium-editor-textarea'})
+            'author':forms.TextInput(attrs={'class':'textinputclass'}),
+            'text':forms.Textarea(attrs={'class':'editable medium-editor-textarea'})
 
         }
--- django_blog/urls.py
+++ django_blog/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.conf.urls import url, include
+from blog import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
